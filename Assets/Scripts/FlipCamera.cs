5211d2a79b157ecb05433264753b23f737f4fbb9
micuat <micuat@aim.com>
2015-02-06 16:23:19 -0500

    Auto scale camera

--- Assets/Scripts/FlipCamera.cs
+++ Assets/Scripts/FlipCamera.cs
@@ -6,10 +6,13 @@ public class FlipCamera : MonoBehaviour {
 	// http://wiki.unity3d.com/index.php?title=InvertCamera
 
 	Matrix4x4 m;
+
 	// Use this for initialization
 	void Start () {
 		m = Matrix4x4.identity;
 		m [1, 1] = -1;
+
+		camera.transform.position = new Vector3 (Screen.width / 2.0f, Screen.height / 2.0f, 0);
 	}
 	
 	// Update is called once per frame
