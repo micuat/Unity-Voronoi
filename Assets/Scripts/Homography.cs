6ddab82e859751671d53aedc28849dd786818d51
micuat <micuat@aim.com>
2015-02-06 12:00:54 -0500

    Manual viewport adjustment

--- Assets/Scripts/Homography.cs
+++ Assets/Scripts/Homography.cs
@@ -33,6 +33,9 @@ public class Homography : MonoBehaviour {
 	public string fileName;
 
 	public Vector3 offset;
+
+	public bool allowManualOffset = false;
+	Vector2 manualOffset; 
 	
 	// Use this for initialization
 	void Start () {
@@ -73,7 +76,20 @@ public class Homography : MonoBehaviour {
 	// Update is called once per frame
     void Update()
     {
-
+		if(allowManualOffset) {
+			if (Input.GetKeyDown (KeyCode.A)) {
+				transform.position -= new Vector3(1, 0, 0);
+			}
+			if (Input.GetKeyDown (KeyCode.D)) {
+				transform.position += new Vector3(1, 0, 0);
+			}
+			if (Input.GetKeyDown (KeyCode.W)) {
+				transform.position -= new Vector3(0, 1, 0);
+			}
+			if (Input.GetKeyDown (KeyCode.S)) {
+				transform.position += new Vector3(0, 1, 0);
+			}
+		}
     }
 
 //	void OnDrawGizmos()
